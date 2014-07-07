97df29af512a5b56e55dab304680fff1d68bf74e
Luke McComb <lucasmccomb@gmail.com>
2014-07-07 06:52:41 -0600

    visitor can click register button to view register form with username, password and submit

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -5,4 +5,10 @@ feature "visitor can visit home page" do
   scenario "visitor can see registration button" do
     expect(page).to have_content("Register")
   end
+  scenario "visitor can click registration button to view registration form" do
+    click_on "Register"
+    expect(page).to have_content("Username:")
+    expect(page).to have_content("Password:")
+    expect(page).to have_button("Submit")
+  end
 end
